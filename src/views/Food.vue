cc675568926476fece44ab08ad4d2b7c2b87d16a
Wendy Fang <fun19111@gmail.com>
2025-04-25 17:29:08 +0800

    fix: add vue-router

--- src/views/Food.vue
+++ src/views/Food.vue
@@ -62,7 +62,8 @@ function flyToRestaurant(lat, lng, name) {
   }
 }
 
-import { fooddata as foodData } from '../../data/foodData.js'
+import { fooddata as foodData } from '../data/foodData.js'
+
 
 const defaultImg = 'https://via.placeholder.com/300x200?text=Coming+Soon'
 </script>
