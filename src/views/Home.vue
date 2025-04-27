6fdb227cec76d2ae9857d1c8ec11ac782ad24da0
Wendy Fang <fun19111@gmail.com>
2025-04-27 17:16:47 +0800

    fix: add vue-router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,35 +3,58 @@
 
 <template>
   <div class="home-wrapper">
-    <h1>溫蒂的日本旅遊網站</h1>
-    <p>這是一趟甜甜和風之旅，快來看看行程和美食吧！</p>
-    <p class="emoji-hint">🌸 東京｜🍣 美食｜🗾 行程｜⛩️ 京都</p>
+    <h1>司馬庫斯團｜日本東京GO!</h1>
+    <p class="emoji-hint">🌸 Tokiyo｜🗻 Mount Fuji </p>
   </div>
+
+  <div class="slideshow">
+      <img
+        :src="images[currentImage]"
+        class="slide"
+        alt="輪播圖片"
+      />
+    </div>
+
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const images = [
+  '/images/slide1.jpg',
+  '/images/slide2.jpg',
+  '/images/slide3.jpg',
+  '/images/slide4.jpg',
+  '/images/slide5.jpg',
+  '/images/slide6.jpg',
+  '/images/slide7.jpg',
+  '/images/slide8.jpg'
+  
+]
+
+const currentImage = ref(0)
+
+onMounted(() => {
+  setInterval(() => {
+    currentImage.value = (currentImage.value + 1) % images.length
+  }, 3000) // 每3秒換一張
+})
+</script>
+
 <style scoped>
-.home-wrapper {
-  text-align: center;
-  padding: 100px 20px;
-  background-color: #fffafc;
-  color: #444;
-  font-family: "Noto Sans TC", sans-serif;
+.slideshow {
+  width: 100%;
+  max-width: 600px;
+  height: 350px;
+  margin: 20px auto;
+  overflow: hidden;
+  position: relative;
 }
-
-h1 {
-  color: #f6d5d8;
-  font-size: 2.4rem;
-  margin-bottom: 20px;
-}
-
-p {
-  font-size: 1.2rem;
-  margin: 10px 0;
-}
-
-.emoji-hint {
-  font-size: 1rem;
-  color: #a6dce3;
-  margin-top: 20px;
+.slide {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 12px;
+  transition: opacity 1s ease-in-out;
 }
 </style>
